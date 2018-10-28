f19cce8bfe62de43b6605ce1bf8f4943e47f408b
Howard <howard.li@duke.edu>
2018-10-27 21:18:42 -0400

    HL232/ Adding Sphinx files?

---
+++ _build/html/_sources/docs/modules.rst.txt
@@ -0,0 +1,10 @@
+bme590hrm
+=========
+
+.. toctree::
+   :maxdepth: 4
+
+   HeartRateMonitor
+   conf
+   detect_peaks
+   test_HeartRateMonitor
--- conf.py
+++ conf.py
@@ -26,7 +26,7 @@ author = 'Howard Li'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '1.0.2'
 
 
 # -- General configuration ---------------------------------------------------
